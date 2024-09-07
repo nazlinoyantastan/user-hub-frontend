bf73dc28308a0e38727780bf002324617a5641f2
nazlinoyantastan <nazlinoyan_88@hotmail.com>
2024-09-07 13:46:49 +0300

    240907-v3

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,8 +1,8 @@
 // photo.service.ts
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { map, switchMap } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { catchError, map, switchMap } from 'rxjs/operators';
 import { environment } from '../../environments/environment';
 
 @Injectable({
@@ -19,7 +19,11 @@ export class PhotoService {
     return this.http.get<any[]>(`${this.apiUrl}/users/${userId}/albums`).pipe(
       map(albums => albums[0]?.id),
       switchMap(albumId => this.http.get<any[]>(`${this.apiUrl}/albums/${albumId}/photos`)),
-      map(photos => photos[0]?.thumbnailUrl || 'path/to/default/image.jpg')
+      map(photos => photos[0]?.thumbnailUrl || '../../assets/userphoto.png'),
+      catchError(error => {
+        console.error('An error occurred:', error);        
+        return of('../../assets/userphoto.png'); 
+      })
     );
   }
 }
